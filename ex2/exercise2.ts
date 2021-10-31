440ab06887a0edf1d2685bba386abab317d9081f
Raquel Valadão <raqueldepapel@gmail.com>
2021-10-31 17:03:34 -0300

    pr 2

--- ex2/exercise2.ts
+++ ex2/exercise2.ts
@@ -6,7 +6,7 @@ type Object = {
     bio: string
 };
 
-var list: Array<Object> = Array(
+const list: Array<Object> = Array(
     {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
     {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
     {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
@@ -14,19 +14,19 @@ var list: Array<Object> = Array(
 );
 
 //create a function that returns the bio given an id
-function searchForBioGivenId(givenId: number, people: Array<Object>){
+function searchForBioGivenId(givenId: number, people: Array<Object>): string {
     return people.filter(person => person.id === givenId)[0].bio     
 }
 
 //create a function that returns the name given an id
-function searchForNameGivenId(givenId: number, people: Array<Object>){
+function searchForNameGivenId(givenId: number, people: Array<Object>): string {
     return people.filter(person => person.id === givenId)[0].name
 }
 
 //create a function that deletes item given an id
-function deleteItemGivenId(givenId: number, people: Array<Object>){
-    let personToDelete = people.filter(person => person.id === givenId)[0]
-    const index = people.indexOf(personToDelete)
+function deleteItemGivenId(givenId: number, people: Array<Object>): void {
+    let personToDelete: Object = people.filter(person => person.id === givenId)[0]
+    const index: number = people.indexOf(personToDelete)
     people.splice(index, 1)    
 }
 
